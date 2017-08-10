aeb3aead1dc172f8ed84fe58cce857fc7f6778d9
Robbin Habermehl <robbin@habermehl.net>
2017-08-10 15:53:36 +0200

    Add Redux Devtools support

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import { render } from 'react-dom'
 import { createStore } from 'redux'
+import { devToolsEnhancer } from 'redux-devtools-extension/logOnlyInProduction';
 import { Provider } from 'react-redux'
+
 import App from './components/App'
 import reducer from './reducers'
 
-const store = createStore(reducer)
+const store = createStore(reducer, devToolsEnhancer())
 
 render(
   <Provider store={store}>
