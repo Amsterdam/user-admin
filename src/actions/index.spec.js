0c05f5f95e69f88777a4ec12d95b6f48899062fa
Boris Arkenaar <boris@arkenaar.com>
2017-09-26 13:38:05 +0200

    Fixed existing unit tests

--- src/actions/index.spec.js
+++ src/actions/index.spec.js
@@ -1,17 +1,8 @@
-import * as actions from './index';
+import setVisibilityFilter from './index';
 
 describe('user actions', () => {
-  it('addUser should create ADD_USER action', () => {
-    expect(actions.addUser({emailAddress: 'john@doe.com', name: 'John Doe'})).toEqual({
-      type: 'ADD_USER',
-      id: 2,
-      emailAddress: 'john@doe.com',
-      name: 'John Doe'
-    });
-  });
-
   it('setVisibilityFilter should create SET_VISIBILITY_FILTER action', () => {
-    expect(actions.setVisibilityFilter('active')).toEqual({
+    expect(setVisibilityFilter('active')).toEqual({
       type: 'SET_VISIBILITY_FILTER',
       filter: 'active'
     });
--- src/reducers/accounts.spec.js
+++ src/reducers/accounts.spec.js
@@ -4,50 +4,83 @@ describe('accounts reducer', () => {
   it('should handle initial state', () => {
     expect(
       accounts(undefined, {}).length
-    ).toBe(2);
+    ).toBe(0);
   });
 
-  it('should handle ADD_TODO', () => {
-    expect(
-      accounts([], {
-        type: 'ADD_USER',
-        emailAddress: 'john@doe.com',
-        name: 'John Doe',
-        id: 0
-      })
-    ).toEqual([
-      {
-        emailAddress: 'john@doe.com',
-        name: 'John Doe',
-        id: 0
-      }
-    ]);
+  describe('FETCH_ACCOUNT_SUCCESS', () => {
+    it('adds an account to an empty state', () => {
+      expect(
+        accounts([], {
+          type: 'FETCH_ACCOUNT_SUCCESS',
+          account: {
+            emailAddress: 'john@doe.com',
+            name: 'John Doe',
+            id: 0
+          }
+        })
+      ).toEqual([
+        {
+          emailAddress: 'john@doe.com',
+          name: 'John Doe',
+          id: 0
+        }
+      ]);
+    });
 
-    expect(
-      accounts([
+    it('adds an account to a non-empty state', () => {
+      expect(
+        accounts([
+          {
+            emailAddress: 'john@doe.com',
+            name: 'John Doe',
+            active: true,
+            id: 0
+          }
+        ], {
+          type: 'FETCH_ACCOUNT_SUCCESS',
+          account: {
+            emailAddress: 'jane@doe.com',
+            name: 'Jane Doe',
+            id: 1
+          }
+        })
+      ).toEqual([
         {
           emailAddress: 'john@doe.com',
           name: 'John Doe',
           active: true,
           id: 0
+        }, {
+          emailAddress: 'jane@doe.com',
+          name: 'Jane Doe',
+          id: 1
         }
-      ], {
-        type: 'ADD_USER',
-        emailAddress: 'jane@doe.com',
-        name: 'Jane Doe',
-        id: 1
-      })
-    ).toEqual([
-      {
-        emailAddress: 'john@doe.com',
-        name: 'John Doe',
-        active: true,
-        id: 0
-      }, {
-        emailAddress: 'jane@doe.com',
-        name: 'Jane Doe',
-        id: 1
-      }
-    ]);
+      ]);
+    });
+
+    it('overwrites an existing account', () => {
+      expect(
+        accounts([
+          {
+            emailAddress: 'john@doe.com',
+            name: 'John Doe',
+            id: 0
+          }
+        ], {
+          type: 'FETCH_ACCOUNT_SUCCESS',
+          account: {
+            emailAddress: 'john@doe.com',
+            name: 'Jane Doe',
+            id: 1
+          }
+        })
+      ).toEqual([
+        {
+          emailAddress: 'john@doe.com',
+          name: 'Jane Doe',
+          id: 1
+        }
+      ]);
+    });
   });
 });
